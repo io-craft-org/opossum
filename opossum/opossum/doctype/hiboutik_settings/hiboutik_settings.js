41ebb5cf9ea1c38092c27d504ba9a7809224e2e4
Guillaume Libersat <glibersat@sigill.org>
2021-05-07 15:05:48 +0200

    Create Hiboutik Settings doctype

---
+++ opossum/opossum/doctype/hiboutik_settings/hiboutik_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, ioCraft and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Hiboutik Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
