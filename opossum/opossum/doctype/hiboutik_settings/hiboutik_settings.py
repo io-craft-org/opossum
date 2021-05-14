9a17dbfa6cccca935b95e81a2f4462e86a50aa8b
Guillaume Libersat <glibersat@sigill.org>
2021-05-14 15:15:52 +0200

    hiboutik settings  - rename field is_enable to enable_sync  - create custom fields in Item upong synchronization activation  - fix and improve tests

--- opossum/opossum/doctype/hiboutik_settings/hiboutik_settings.py
+++ opossum/opossum/doctype/hiboutik_settings/hiboutik_settings.py
@@ -7,6 +7,7 @@ from __future__ import unicode_literals
 import json
 
 import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_field
 from frappe.model.document import Document
 from opossum.opossum.hiboutik import HiboutikConnector
 from opossum.opossum.model import Item
@@ -16,9 +17,10 @@ from six import string_types
 class HiboutikSettings(Document):
     def validate(self):
         self.validate_settings()
+        self.create_delete_custom_fields()
 
     def validate_settings(self):
-        if self.is_enabled:
+        if self.enable_sync:
             if not self.instance_name:
                 frappe.throw(_("Please enter an instance name"))
 
@@ -28,9 +30,38 @@ class HiboutikSettings(Document):
             if not self.api_key:
                 frappe.throw(_("Please enter an API key"))
 
+    def create_delete_custom_fields(self):
+        """When the user enables sync, make custom fields in Doctypes"""
+
+        if self.enable_sync:
+            custom_fields = {}
+
+            for doctype in ["Item"]:
+                fields = [
+                    dict(
+                        fieldname="hiboutik_id",
+                        label="Hiboutik ID",
+                        fieldtype="Data",
+                        read_only=1,
+                        print_hide=1,
+                        translatable=0,
+                    ),
+                    dict(
+                        fieldname="sync_with_hiboutik",
+                        label="Sync with Hiboutik",
+                        fieldtype="Check",
+                        insert_after="is_stock_item",
+                        print_hide=1,
+                    ),
+                ]
+                for df in fields:
+                    create_custom_field(doctype, df)
+
 
 @frappe.whitelist()
 def sync_item(json_doc):
+    """Given an Item, request sync to POS"""
+
     if isinstance(json_doc, string_types):
         item_doc = json.loads(json_doc)
     else:
@@ -40,7 +71,7 @@ def sync_item(json_doc):
 
     hiboutik_settings = frappe.get_doc("Hiboutik Settings")
 
-    if not hiboutik_settings.is_enabled:
+    if not hiboutik_settings.enable_sync:
         return False
 
     hiboutik = HiboutikConnector(
@@ -52,3 +83,19 @@ def sync_item(json_doc):
     hiboutik.sync(item)
 
     return True
+
+
+@frappe.whitelist
+def pos_invoice_webhook():
+    """Receives a POS Invoice from Hiboutik"""
+    # Check if pos-opening-entry exists, create it otherwise
+    # XXX: we need a default POS Profile (set that in Hiboutik Settings)
+
+    opening_entry = frappe.get_doc("POS Opening Entry")
+
+    # Insert a POS Invoice with update stock selected
+
+
+@frappe.whitelist
+def pos_closing_webhook():
+    pass
--- opossum/opossum/doctype/hiboutik_settings/test_hiboutik_settings.py
+++ opossum/opossum/doctype/hiboutik_settings/test_hiboutik_settings.py
@@ -19,35 +19,41 @@ test_records = frappe.get_test_records("Item")
 class TestHiboutikSettings(unittest.TestCase):
     def test_sync_one_item_if_disabled(self):
         settings = frappe.get_doc("Hiboutik Settings")
-        settings.is_enabled = False
+        settings.enable_sync = False
         settings.save()
 
-        article_json = json.JSONEncoder().encode({"item_code": "article1", "name": "An article"})
+        article_json = json.JSONEncoder().encode(
+            {"item_code": "article1", "name": "An article"}
+        )
         assert sync_item(article_json) == False
 
     def test_sync_one_item_with_wrong_credentials(self):
         settings = frappe.get_doc("Hiboutik Settings")
-        settings.is_enabled = True
+        settings.enable_sync = True
         settings.instance_name = "wrong instance name"
         settings.username = "wrong username"
         settings.api_key = "wrong api key"
         settings.save()
 
         with patch.object(HiboutikConnector, "sync", return_value=False) as mock_method:
-            article_json = json.JSONEncoder().encode({"item_code": "article1", "name": "An article"})
+            article_json = json.JSONEncoder().encode(
+                {"item_code": "article1", "name": "An article"}
+            )
             assert sync_item(article_json) == False
             mock_method.assert_called_once()
 
     def test_sync_one_item(self):
         settings = frappe.get_doc("Hiboutik Settings")
-        settings.is_enabled = True
+        settings.enable_sync = True
         settings.instance_name = "valid instance name"
         settings.username = "valid username"
         settings.api_key = "valid api key"
         settings.save()
 
         with patch.object(HiboutikConnector, "sync", return_value=True) as mock_method:
-            article_json = json.JSONEncoder().encode({"item_code": "article1", "name": "An article"})
+            article_json = json.JSONEncoder().encode(
+                {"item_code": "article1", "name": "An article"}
+            )
             assert sync_item(article_json) == True
             mock_method.assert_called_once()
 
@@ -60,3 +66,15 @@ class TestHiboutikSettings(unittest.TestCase):
         assert item.name == doc.name
         assert item.price == 10.0
         assert item.vat == 1
+
+    def test_enabling_hiboutik_create_custom_item_fields(self):
+        settings = frappe.get_doc("Hiboutik Settings")
+        settings.enable_sync = True
+        settings.instance_name = "random instance name"
+        settings.username = "random username"
+        settings.api_key = "random api key"
+        settings.save()
+
+        item_dt = frappe.get_meta("Item")
+
+        assert item_dt.has_field("hiboutik_id") is True
