d23653bbdb998865bfac0b370de599ab4499208d
Marc-Antoine Dupré <marc-antoine.dupre+git@protonmail.com>
2021-07-15 12:51:52 +0200

    Fix connection errors in tests by mocking webhook method
    Mock the `set_sale_webhook` from the Hiboutik connector to avoid failed connection exceptions when saving an Item document in test setups.

--- opossum/opossum/doctype/hiboutik_settings/test_hiboutik_settings.py
+++ opossum/opossum/doctype/hiboutik_settings/test_hiboutik_settings.py
@@ -83,57 +83,59 @@ class TestHiboutikSettings(unittest.TestCase):
         assert sync_item(article_json) == False
 
     def test_sync_one_item_with_wrong_credentials(self):
-        settings = frappe.get_doc("Hiboutik Settings")
-        settings.enable_sync = True
-        settings.instance_name = "wrong_instance_name"
-        settings.username = "wrong username"
-        settings.api_key = "wrong api key"
-        settings.pos_profile = "_Opossum Hiboutik"
-        settings.save()
+        with patch.object(HiboutikConnector, "set_sale_webhook"):
+            settings = frappe.get_doc("Hiboutik Settings")
+            settings.enable_sync = True
+            settings.instance_name = "wrong_instance_name"
+            settings.username = "wrong username"
+            settings.api_key = "wrong api key"
+            settings.pos_profile = "_Opossum Hiboutik"
+            settings.save()
 
-        with patch.object(HiboutikConnector, "sync") as mock_method:
-            mock_method.side_effect = HiboutikAPIError()
-            article_json = json.JSONEncoder().encode(
-                {
-                    "item_code": "_opossum_item1",
-                    "name": "An article",
-                    "hiboutik_id": "T_Item1",
-                }
-            )
+            with patch.object(HiboutikConnector, "sync") as mock_method:
+                mock_method.side_effect = HiboutikAPIError()
+                article_json = json.JSONEncoder().encode(
+                    {
+                        "item_code": "_opossum_item1",
+                        "name": "An article",
+                        "hiboutik_id": "T_Item1",
+                    }
+                )
 
-            with self.assertRaises(HiboutikAPIError) as context:
-                sync_item(article_json)
+                with self.assertRaises(HiboutikAPIError) as context:
+                    sync_item(article_json)
 
     def test_sync_one_item(self):
-        settings = frappe.get_doc("Hiboutik Settings")
-        settings.enable_sync = True
-        settings.instance_name = "valid_instance_name"
-        settings.username = "valid username"
-        settings.api_key = "valid api key"
-        settings.pos_profile = "_Opossum Hiboutik"
-        settings.save()
+        with patch.object(HiboutikConnector, "set_sale_webhook"):
+            settings = frappe.get_doc("Hiboutik Settings")
+            settings.enable_sync = True
+            settings.instance_name = "valid_instance_name"
+            settings.username = "valid username"
+            settings.api_key = "valid api key"
+            settings.pos_profile = "_Opossum Hiboutik"
+            settings.save()
 
-        item = Item(
-            code="_opossum_item1",
-            name="An Article",
-            price="10",
-            vat=1,
-            is_stock_item=False,
-            external_id="T_Item1",
-        )
-
-        with patch.object(HiboutikConnector, "sync", return_value=item) as mock_method:
-            article_json = json.JSONEncoder().encode(
-                {
-                    "item_code": item.code,
-                    "name": item.name,
-                    "hiboutik_id": item.external_id,
-                }
+            item = Item(
+                code="_opossum_item1",
+                name="An Article",
+                price="10",
+                vat=1,
+                is_stock_item=False,
+                external_id="T_Item1",
             )
-            assert sync_item(article_json) == True
-            doc = frappe.get_doc("Item", item.code)
-            assert doc.hiboutik_id == item.external_id
-            mock_method.assert_called_once()
+
+            with patch.object(HiboutikConnector, "sync", return_value=item) as mock_method:
+                article_json = json.JSONEncoder().encode(
+                    {
+                        "item_code": item.code,
+                        "name": item.name,
+                        "hiboutik_id": item.external_id,
+                    }
+                )
+                assert sync_item(article_json) == True
+                doc = frappe.get_doc("Item", item.code)
+                assert doc.hiboutik_id == item.external_id
+                mock_method.assert_called_once()
 
     def test_model_is_fed_from_item(self):
         doc = frappe.get_doc("Item", "_opossum_item1")
@@ -146,17 +148,18 @@ class TestHiboutikSettings(unittest.TestCase):
         assert item.vat == 1
 
     def test_enabling_hiboutik_create_custom_item_fields(self):
-        settings = frappe.get_doc("Hiboutik Settings")
-        settings.enable_sync = True
-        settings.instance_name = "random_instance_name"
-        settings.username = "random username"
-        settings.api_key = "random api key"
-        settings.pos_profile = "_Opossum Hiboutik"
-        settings.save()
+        with patch.object(HiboutikConnector, "set_sale_webhook"):
+            settings = frappe.get_doc("Hiboutik Settings")
+            settings.enable_sync = True
+            settings.instance_name = "random_instance_name"
+            settings.username = "random username"
+            settings.api_key = "random api key"
+            settings.pos_profile = "_Opossum Hiboutik"
+            settings.save()
 
-        item_dt = frappe.get_meta("Item")
+            item_dt = frappe.get_meta("Item")
 
-        assert item_dt.has_field("hiboutik_id") is True
+            assert item_dt.has_field("hiboutik_id") is True
 
 
 class TestHiboutikWebhooks(unittest.TestCase):
